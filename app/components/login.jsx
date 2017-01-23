7395962cbe1641a7ff4b4d66328cfe60b485ab3e
Prateek Agarwal <prateekagr98@gmail.com>
2017-01-23 12:08:00 +0530

    minor modifications

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -3,30 +3,30 @@
 import React from 'react';
 
 class Login extends React.Component{
-	constructor(props){
-		super(props);
-	}
+  constructor(props){
+    super(props);
+  }
 
-	render(){
+  render(){
 
-		return (
-			<div className="u-c-full-h">
-				<div className="c-card">
-					<p className="c-card__heading">
-						Enter mobile number
-					</p>
-					<div className="pure-u-1-1 c-section">
-						<input type="text" className="c-input--clear" placeholder="+91 123456789" autofocus />
-					</div>
-					<div className="pure-u-1-1 c-section--large">
-						<button type="submit" className="u-t-center c-btn--primary">
-							CONTINUE
-						</button>
-					</div>
-				</div>
-			</div>
-		)
-	}
+    return (
+      <div className="u-c-full-h">
+        <div className="c-card--full u-pos-has">
+          <p className="c-card__heading">
+            Enter mobile number
+          </p>
+          <div className="pure-u-1-1 c-section">
+            <input type="text" className="c-input--clear" placeholder="+91 123456789" autofocus />
+          </div>
+          <div className="pure-u-22-24 c-section--large u-pos u-pos-m-b">
+            <button type="submit" className="u-t-center c-btn--primary">
+              CONTINUE
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 };
 
 export default Login;
