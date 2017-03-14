39a1a195defeb7228f521da975af316bb65e79fc
Prateek Agarwal <prateekagr98@gmail.com>
2017-03-14 22:18:10 +0530

    genre design change

--- app/components/genre_selection.jsx
+++ app/components/genre_selection.jsx
@@ -1,63 +1,83 @@
 'use strict';
 
 import React from 'react';
+import { map as _map } from 'lodash';
 
 class GenreSelection extends React.Component{
 	constructor(props) {
 		super(props);
+
+    this.state = {
+      genre_list : [
+        {
+          id: 1,
+          name: 'Fiction',
+          description: 'This is the description',
+          selected: false
+        },
+        {
+          id: 2,
+          name: 'Sc-fi',
+          description: 'This is the description',
+          selected: false
+        }
+      ]
+    }
 	}
+
+  toggleSelection(id){
+    let genre_list;
+
+    genre_list = _map(this.state.genre_list, function(item){
+      if(item.id === id){
+        item.selected = !item.selected;
+      }
+
+      return item;
+    });
+
+    this.setState({
+      genre_list
+    });
+  }
+
+  getGenreLayout(){
+    let __self, __layouts;
+
+    __self = this;
+
+    __layouts = _map(this.state.genre_list, function(item, index){
+      let activeClass = item.selected ? 'active' : '';
+
+      return (
+        <div
+          key={index}
+          className="c-g-s__item pure-u-sm-1 u-pos-has"
+          onClick={__self.toggleSelection.bind(__self, item.id)}>
+          <div className="c-g-s__item--content">
+            <p className="c-g-s__name">{item.name}</p>
+            <p className="c-g-s__caption">{item.description}</p>
+          </div>
+          <div className={`c-g-s__item--action u-pos-r-m ${activeClass}`}>
+            <i className="ion-ios-checkmark"></i>
+          </div>
+        </div>
+      );
+    });
+
+    return __layouts;
+  }
 
 	render() {
 
 		return(
 			<div>
-        <div className="c-card--half u-pos-has">
+        <div className="c-card--clear">
           <p className="c-card__heading u-cushion-b pure-u-2-3">
             Select your favourite genre
           </p>
-          <p className="c-card__heading pure-u-1-3">
-            -&gt;
-          </p>
-          <div className="c-g-s__item selected">
-            Fiction
-          </div>
-        </div>
-        <div className="c-g-s u-cushion-all">
-          <div className="c-g-s__item">
-            Sci-fi
-          </div>
-          <div className="c-g-s__item">
-            Satire
-          </div>
-          <div className="c-g-s__item">
-            Comics
-          </div>
-          <div className="c-g-s__item">
-            Art
-          </div>
-          <div className="c-g-s__item">
-            Sci-fi
-          </div>
-          <div className="c-g-s__item">
-            Satire
-          </div>
-          <div className="c-g-s__item">
-            Comics
-          </div>
-          <div className="c-g-s__item">
-            Art
-          </div>
-          <div className="c-g-s__item">
-            Sci-fi
-          </div>
-          <div className="c-g-s__item">
-            Satire
-          </div>
-          <div className="c-g-s__item">
-            Comics
-          </div>
-          <div className="c-g-s__item">
-            Art
+          <div className="c-g-s pure-u-1">
+            {this.getGenreLayout()}
           </div>
         </div>
       </div>
